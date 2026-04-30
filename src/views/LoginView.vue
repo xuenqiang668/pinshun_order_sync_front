c1594fe3c3d5bb549695514e334ca20e4bec7716
gj <guojun@qq.com>
2026-04-30 08:44:36 +0800

    fix: 格式化

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -43,28 +43,14 @@ const onSubmit = async () => {
       <template #header>
         <div class="text-center text-xl font-semibold text-slate-700">品瞬科技系统登录</div>
       </template>
-      <el-form
-        ref="formRef"
-        :model="form"
-        :rules="rules"
-        label-position="top"
-        @keyup.enter="onSubmit"
-      >
+      <el-form ref="formRef" :model="form" :rules="rules" label-position="top" @keyup.enter="onSubmit">
         <el-form-item label="用户名" prop="username">
           <el-input v-model.trim="form.username" placeholder="请输入用户名" clearable />
         </el-form-item>
         <el-form-item label="密码" prop="password">
-          <el-input
-            v-model.trim="form.password"
-            type="password"
-            show-password
-            placeholder="请输入密码"
-            clearable
-          />
+          <el-input v-model.trim="form.password" type="password" show-password placeholder="请输入密码" clearable />
         </el-form-item>
-        <el-button type="primary" class="w-full" :loading="loading" @click="onSubmit"
-          >登录</el-button
-        >
+        <el-button type="primary" class="w-full" :loading="loading" @click="onSubmit">登录</el-button>
       </el-form>
     </el-card>
   </div>
